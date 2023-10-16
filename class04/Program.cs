a5999e8da5306aa91087d4739c1a5e6dcf4beb8d
student <student@201-53>
2023-10-16 11:24:01 +0800

    加入專案檔案。

---
+++ class04/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace class04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("");
+            if (int.TryParse(Console.ReadLine(), out int number))
+            {
+                Console.Write($"The factors of {number}: ");
+                FindFactors(number);
+            }
+            else
+            {
+                Console.WriteLine("無效輸入，請輸入一個整數。");
+            }
+        }
+
+        static void FindFactors(int number)
+        {
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    Console.Write(i);
+                    if (i < number)
+                    {
+                        Console.Write(",");
+                    }
+                }
+            }
+            Console.WriteLine(); 
+        }
+    }
+}
+    
